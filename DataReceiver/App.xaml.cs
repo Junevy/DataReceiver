c1c8572ea43776e2bc680e7ce415d5d235014d60
Junevy <junev.chung@gmail.com>
2025-09-24 21:11:48 +0800

    添加项目文件。

---
+++ DataReceiver/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using DataReceiver.ViewModels;
+using DataReceiver.Views;
+using HandyControl.Tools;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DataReceiver
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public static new App Current = (App)Application.Current;
+        public IServiceProvider Services;
+        //private Style;
+
+        public App()
+        {
+            var container = new ServiceCollection();
+            container.AddSingleton<MainView>();
+            container.AddSingleton<MainViewModels>();
+            container.AddSingleton<DataView>();
+            container.AddSingleton<DataViewModel>(); 
+            container.AddSingleton<SocketView>();
+            container.AddSingleton<SocketViewModel>();
+
+            Services = container.BuildServiceProvider();
+        }
+
+        public Style? LoadResource(string styleName)
+        {
+            return Application.Current.Resources[styleName] as Style;
+        }
+
+        public Style? LoadResource<T>()
+        {
+            return Application.Current.Resources[typeof(T)] as Style;
+        }
+    }
+
+}
---
+++ DataReceiver/Helper/Control/NavigationListBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DataReceiver.Helper.Control
+{
+    public sealed class NavigationListBox : ListView
+    {
+        private Image _icon;
+        private string _content;
+
+    }
+}
