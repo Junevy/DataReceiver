7de72842b1f40d3735c11638f2d686506c8ed65e
Junevy <junev.chung@gmail.com>
2025-12-06 00:39:05 +0800

    更新了一些bug。

--- DataReceiver/Models/TaskScheduleConfig.cs
+++ DataReceiver/Models/TaskScheduleConfig.cs
@@ -8,13 +8,16 @@ namespace DataReceiver.Models.Socket.Config
     {
         public event Action<bool>? OnStateChanged;
 
-        [ObservableProperty]
-        private bool isEdited = false;
+        //[ObservableProperty]
+        //private bool isEdited = false;
 
         [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(CanEdit))]
+        [NotifyPropertyChangedFor(nameof(CanEdit))] // Notify the CanEdit when IsEnable changed.
         private bool isEnable = false;
 
+        /// <summary>
+        /// Expresstion some textbox can edit when IsEnable is false 
+        /// </summary>
         public bool CanEdit => !IsEnable;
 
         [ObservableProperty]
@@ -25,8 +28,8 @@ namespace DataReceiver.Models.Socket.Config
         [ObservableProperty]
         private string arguments = string.Empty;
 
-        [ObservableProperty]
-        private string workingDirectory = string.Empty;
+        //[ObservableProperty]
+        //private string workingDirectory = string.Empty;
 
         [ObservableProperty]
         [Required(ErrorMessage = "任务计划的名称不能为空！")]
@@ -41,12 +44,20 @@ namespace DataReceiver.Models.Socket.Config
         [NotifyDataErrorInfo]
         private string author = "DataReceiverApp";
 
+        /// <summary>
+        /// Gets or sets the number of days between scheduled task executions.
+        /// </summary>
+        /// <remarks>The value must be greater than 0 and less than 23. Validation attributes ensure that
+        /// the property cannot be set to an invalid value.</remarks>
         [ObservableProperty]
         [Required(ErrorMessage = "任务计划的执行间隔天数不能为空！")]
         [RegularExpression(@"^(?:0?[1-9]|1\d|2[0-3])$", ErrorMessage = "间隔天数必须大于0天并且小于23天")]
         [NotifyDataErrorInfo]
         private short intervalDays = 1;
-        
+
+        /// <summary>
+        /// Execution start time boundary
+        /// </summary>
         [ObservableProperty]
         private TimeSpan startBoundary = TimeSpan.FromHours(4); // 默认每天凌晨2点执行
 
